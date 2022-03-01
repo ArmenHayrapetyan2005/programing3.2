77450d70357f762bb2af6e776a4b6f2f935ddc6e
ArmenHayrapetyan2005 <armen.h24.y@tumo.org>
2022-03-01 19:58:08 +0400

    dsgdsg

---
+++ Game/Predator1.js
@@ -0,0 +1,91 @@
+class Predator1 extends LivingCreature {
+    constructor(x, y) {
+        super(x, y);
+        this.energy = 12
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        return super.chooseCell(character);
+    }
+
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        if (newCell && this.multiply >= 14) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 5;
+
+            var newPr = new Predator1(newX, newY);
+            Predator1Arr.push(newPr);
+            this.multiply = 0;
+        }
+    }
+
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+        if (newCell && this.energy >= 0) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        } else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(4);
+        var newCell = random(emptyCells);
+        if (newCell) {
+            this.energy++
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in waterArr) {
+                if (newX == waterArr[i].x && newY == waterArr[i].y) {
+                    waterArr.splice(i, 1);
+                    break;
+                }
+            }
+
+        } else {
+            this.move()
+        }
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0
+        for (var i in Predator1Arr) {
+            if (this.x == Predator1Arr[i].x && this.y == Predator1Arr[i].y) {
+                Predator1Arr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
---
+++ Game/Water.js
@@ -0,0 +1,19 @@
+class Water extends LivingCreature {
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        return super.chooseCell(character);
+    }
+}
